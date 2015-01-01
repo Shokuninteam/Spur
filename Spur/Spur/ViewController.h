a7f646698804cd5bc200582b6f71b17dae57ab7b
LittleCatBear <j.huguet9@gmail.com>
2015-01-01 20:36:36 +1300

    setup Spur project

---
+++ Spur/Spur/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spur
+//
+//  Created by Julie Huguet on 01/01/2015.
+//  Copyright (c) 2015 Shokunin-Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
